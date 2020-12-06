973ba2bc5144e6aa57773d5e5dcd817e2e5449f4
combbm <combbm@naver.com>
2020-12-06 20:34:00 +0900

    feat: create DirectLink component

---
+++ src/components/DirectLink/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import styled from '@emotion/styled';
+
+interface DirectLinkProps {
+  readonly href: string;
+}
+
+const StyledA = styled.a`
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const DirectLink: React.FC<DirectLinkProps> = ({ href, children }) => {
+  return (
+    <StyledA href={href} target="_blank" rel="noopener noreferrer">
+      {children} <FontAwesomeIcon icon={faArrowRight} size="sm" />
+    </StyledA>
+  );
+};
+
+export default DirectLink;
