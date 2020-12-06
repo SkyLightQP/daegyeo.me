246cba57f92eed6c380cc5b840a0f5699033d8bf
combbm <combbm@naver.com>
2020-12-06 20:07:32 +0900

    feat: add animation effects into Header component and add icons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { motion } from 'framer-motion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faBlog } from '@fortawesome/free-solid-svg-icons';
 
 const TitleContainer = styled.div`
   background-color: #303b4d;
@@ -13,7 +17,7 @@ const TitleContainer = styled.div`
   text-align: center;
 `;
 
-const Title = styled.p`
+const Title = styled(motion.p)`
   font-weight: bold;
   color: white;
   font-size: 3.5rem;
@@ -23,7 +27,7 @@ const Title = styled.p`
   }
 `;
 
-const SubTitle = styled.p`
+const SubTitle = styled(motion.p)`
   color: white;
   font-size: 1.2rem;
 
@@ -32,13 +36,80 @@ const SubTitle = styled.p`
   }
 `;
 
+const IconList = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+
+  color: white;
+
+  margin-top: 2rem;
+
+  cursor: pointer;
+
+  & > a {
+    margin-right: 14px;
+    text-decoration: none;
+    color: inherit;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  & > a:last-child {
+    margin-right: 0;
+  }
+`;
+
+const containerAnimation = {
+  before: {},
+  after: {
+    transition: {
+      ease: 'easeIn',
+      staggerChildren: 0.2,
+      delay: 0.3
+    }
+  }
+};
+
+const textAnimation = {
+  before: {
+    y: 20,
+    opacity: 0
+  },
+  after: {
+    y: 0,
+    opacity: 1
+  }
+};
+
 const Header: React.FC = () => {
   return (
     <TitleContainer>
-      <div>
-        <Title>하늘빛</Title>
-        <SubTitle>Daegyeom Ha</SubTitle>
-      </div>
+      <motion.div initial="before" animate="after" variants={containerAnimation}>
+        <Title variants={textAnimation}>하늘빛</Title>
+        <SubTitle variants={textAnimation}>Daegyeom Ha</SubTitle>
+
+        <IconList>
+          <motion.a
+            href="https://github.com/SkyLightQP/"
+            target="_blank"
+            rel="noopener noreferrer"
+            variants={textAnimation}
+          >
+            <FontAwesomeIcon icon={faGithub} /> GITHUB
+          </motion.a>
+          <motion.a
+            href="https://blog.skylightqp.kr/"
+            target="_blank"
+            rel="noopener noreferrer"
+            variants={textAnimation}
+          >
+            <FontAwesomeIcon icon={faBlog} /> BLOG
+          </motion.a>
+        </IconList>
+      </motion.div>
     </TitleContainer>
   );
 };
