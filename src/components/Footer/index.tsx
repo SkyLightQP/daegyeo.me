72126f749f5f04efc4f3db996a49cd7bff2f67a6
DaegyeomHa <combbm@naver.com>
2022-06-16 19:30:28 +0900

    fix: update content

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,9 +6,10 @@ const Footer: React.FC = () => {
     <>
       <ContentBlock title="">
         <p>
-          이 페이지는 <a href='https://skylightqp.kr'>skylightqp.kr</a> 으로도
+          이 페이지는 <a href='https://skylightqp.kr'><b>skylightqp.kr</b></a> 으로도
           들어올 수 있습니다.
         </p>
+        <p><a href='https://github.com/SkyLightQP/resume/' target="_target"><b>GitHub</b></a> 에서 코드를 확인할 수 있습니다.</p>
       </ContentBlock>
     </>
   );
