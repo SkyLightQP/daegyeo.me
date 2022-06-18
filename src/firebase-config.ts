1b22a28eb3ad751aa710416d453432c967746b97
DaegyeomHa <combbm@naver.com>
2022-06-19 00:47:20 +0900

    fix: create firebase-config and move it

---
+++ src/firebase-config.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBvEKd-lv2_xZKlVpVNXwesxIDIJU--UCw",
+  authDomain: "daegyeome-site.firebaseapp.com",
+  projectId: "daegyeome-site",
+  storageBucket: "daegyeome-site.appspot.com",
+  messagingSenderId: "524135997669",
+  appId: "1:524135997669:web:5f83bbb1b2ded804c5b40c",
+  measurementId: "G-DM8X5N4C0K"
+};
+
+export default firebaseConfig;
--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,15 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBvEKd-lv2_xZKlVpVNXwesxIDIJU--UCw",
-  authDomain: "daegyeome-site.firebaseapp.com",
-  projectId: "daegyeome-site",
-  storageBucket: "daegyeome-site.appspot.com",
-  messagingSenderId: "524135997669",
-  appId: "1:524135997669:web:5f83bbb1b2ded804c5b40c",
-  measurementId: "G-DM8X5N4C0K"
-};
+import firebaseConfig from '../firebase-config';
 
 const firebaseApp = initializeApp(firebaseConfig);
 const firebaseAuth = getAuth(firebaseApp);
