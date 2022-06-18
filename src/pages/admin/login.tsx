42fb5e9fd0bb3a451752dcd7d71ae374cdb06493
DaegyeomHa <combbm@naver.com>
2022-06-19 00:38:20 +0900

    feat: implement login page for admin

---
+++ src/pages/admin/login.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { Title } from '../../components/Typography';
+import firebaseAuth from '../../utils/firebase';
+import VerticalGap from '../../components/VerticalGap';
+
+const Container = styled.div`
+  min-height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  color: #303b4d;
+`;
+
+const Input = styled.input`
+  width: 240px;
+  height: 40px;
+
+  border: 1px solid #e7e7e7;
+  border-radius: 10px;
+
+  padding: 0.5rem 1rem;
+`;
+
+const LoginButton = styled.button`
+  width: 100px;
+  height: 40px;
+
+  color: white;
+  background-color: #303b4d;
+
+  border: none;
+  border-radius: 10px;
+
+  cursor: pointer;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Login: React.FC = () => {
+  const [input, setInput] = useState<{ email: string; password: string; }>({
+    email: '',
+    password: ''
+  });
+  const [err, setError] = useState<string>('');
+
+  const login = async () => {
+    try {
+      const info = await signInWithEmailAndPassword(firebaseAuth, input.email, input.password);
+      localStorage.setItem('accessToken', await info.user.getIdToken());
+    } catch (e) {
+      setError(e.message);
+    }
+  };
+
+  return (
+    <Container>
+      <Title>Admin Login</Title>
+      <VerticalGap gap={16} />
+      <Input
+        type='email'
+        placeholder='Email'
+        value={input.email}
+        onChange={(e) => setInput((prev) => ({ ...prev, email: e.target.value }))}
+      />
+      <VerticalGap gap={10} />
+      <Input
+        type='password'
+        placeholder='Password'
+        value={input.password}
+        onChange={(e) => setInput((prev) => ({ ...prev, password: e.target.value }))}
+        onKeyPress={(e) => {
+          if (e.key === 'Enter') login();
+        }}
+      />
+      <VerticalGap gap={10} />
+      <LoginButton onClick={login}>Login</LoginButton>
+      <VerticalGap gap={16} />
+      <p>{err}</p>
+    </Container>
+  );
+};
+
+export default Login;
