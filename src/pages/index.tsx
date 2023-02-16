62e55b1687cd4195359d0dafa4e63e372781281f
Daegyeom Ha <combbm@gmail.com>
2023-02-16 21:02:50 +0900

    chore: add "remove container" command

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,7 +27,7 @@ const Container = styled.div`
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const { host } = req.headers;
   const protocol = process.env.USE_HTTPS === 'true' ? 'https' : 'http';
-  const res = await Axios.get<{ data: Section[] }>(`http://${host}/api/section`);
+  const res = await Axios.get<{ data: Section[] }>(`${protocol}://${host}/api/section`);
 
   return {
     props: {
