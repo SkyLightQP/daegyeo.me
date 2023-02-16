fb77d382ac98f4ac6d59d51cf1cbb9114493b353
Daegyeom Ha <combbm@gmail.com>
2023-02-16 19:48:14 +0900

    feat: add USE_HTTPS env

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ const Container = styled.div`
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const { host } = req.headers;
-  const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
+  const protocol = process.env.USE_HTTPS === 'true' ? 'https' : 'http';
   const res = await Axios.get<{ data: Section[] }>(`${protocol}://${host}/api/section`);
 
   return {
