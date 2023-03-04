4762773f11f39f08f85723de5bfabb96cda30a25
Daegyeom Ha <combbm@gmail.com>
2023-03-05 00:46:04 +0900

    feat: add script about gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,13 +14,22 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Daegyeom Ha</title>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-J0FQZH7MNY"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-J0FQZH7MNY');
+        </script>
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2490453096003621"
           crossOrigin="anonymous"
         />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Daegyeom Ha</title>
       </Head>
       <ChakraProvider>
         <GlobalStyle />
