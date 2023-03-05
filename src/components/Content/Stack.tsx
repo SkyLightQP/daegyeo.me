bac21a4dc15a51467659c18bb07829ff586ba791
Daegyeom Ha <combbm@gmail.com>
2023-03-06 01:09:03 +0900

    feat: add margin to stack texts

--- src/components/Content/Stack.tsx
+++ src/components/Content/Stack.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Content from '../../database/entity/Content';
+import VerticalGap from '../VerticalGap';
 
 interface StackProps {
   readonly content: Content;
@@ -9,7 +10,9 @@ const ContentStack: React.FC<StackProps> = ({ content }) => {
   if (content.stack) {
     return (
       <>
+        <VerticalGap gap={4} />
         with <b>{content.stack}</b> <br />
+        <VerticalGap gap={4} />
       </>
     );
   }
