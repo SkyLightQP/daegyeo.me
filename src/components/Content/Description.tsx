115b42e75868d55ad443eb75ae52b6d33f9996b1
Daegyeom Ha <combbm@gmail.com>
2023-03-16 22:45:38 +0900

    feat: add markdown viewer instead of span tag

--- src/components/Content/Description.tsx
+++ src/components/Content/Description.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import styled from '@emotion/styled';
 import Content from '../../database/entity/Content';
+
+const MarkdownWrapper = styled.div`
+  li {
+    list-style-position: inside;
+  }
+
+  ul ul {
+    text-indent: 20px;
+  }
+
+  ul ul ul {
+    text-indent: 40px;
+  }
+
+  ul ul ul ul {
+    text-indent: 60px;
+  }
+`;
 
 interface DescriptionProps {
   readonly content: Content;
@@ -8,9 +28,9 @@ interface DescriptionProps {
 const ContentDescription: React.FC<DescriptionProps> = ({ content }) => {
   if (content.description) {
     return (
-      <span style={{ whiteSpace: 'pre-wrap' }}>
-        {content.description} <br />
-      </span>
+      <MarkdownWrapper>
+        <ReactMarkdown>{content.description}</ReactMarkdown>
+      </MarkdownWrapper>
     );
   }
 
