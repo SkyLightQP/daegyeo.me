54f8ae8be2d15f939a6daf4db0a93e93c6598474
Daegyeom Ha <combbm@gmail.com>
2023-05-13 18:05:31 +0900

    fix: move gtag to _app page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import Head from 'next/head';
+import Script from 'next/script';
 import GlobalStyle from '../styles/GlobalStyle';
 import Colors from '../styles/Colors';
 
@@ -17,6 +18,25 @@ const App = ({ Component, pageProps }: AppProps) => {
       <Head>
         <title>daegyeo.me</title>
       </Head>
+      <Script
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2490453096003621"
+        strategy="afterInteractive"
+        crossOrigin="anonymous"
+      />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-J0FQZH7MNY" strategy="afterInteractive" />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+  
+            gtag('config', 'G-J0FQZH7MNY');
+            `
+        }}
+      />
       <ChakraProvider>
         <GlobalStyle />
         <Component {...pageProps} />
