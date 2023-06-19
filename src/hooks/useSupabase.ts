eaaaab6130eecc2a95da7eb08414b28dd79a6e9a
Daegyeom Ha <combbm@gmail.com>
2023-06-19 21:55:45 +0900

    feat: add schema type

--- src/hooks/useSupabase.ts
+++ src/hooks/useSupabase.ts
@@ -1,10 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 import { useMemo } from 'react';
+import { Database } from '../types/schema';
 
 const useSupabase = () => {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL ?? '';
   const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_API_KEY ?? '';
-  return useMemo(() => createClient(supabaseUrl, supabaseKey), [supabaseUrl, supabaseKey]);
+  return useMemo(() => createClient<Database>(supabaseUrl, supabaseKey), [supabaseUrl, supabaseKey]);
 };
 
 export default useSupabase;
