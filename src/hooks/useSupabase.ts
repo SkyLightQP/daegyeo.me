0177f3fd0ce98ca0198dd4f6aa3f806f037001ea
Daegyeom Ha <combbm@gmail.com>
2023-06-20 00:57:02 +0900

    feat: add supabaseClient variable
    * To use in outside of component.

--- src/hooks/useSupabase.ts
+++ src/hooks/useSupabase.ts
@@ -2,10 +2,13 @@ import { createClient } from '@supabase/supabase-js';
 import { useMemo } from 'react';
 import { Database } from '../types/schema';
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL ?? '';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_API_KEY ?? '';
+
 const useSupabase = () => {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL ?? '';
-  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_API_KEY ?? '';
   return useMemo(() => createClient<Database>(supabaseUrl, supabaseKey), [supabaseUrl, supabaseKey]);
 };
+
+export const supabaseClient = createClient<Database>(supabaseUrl, supabaseKey);
 
 export default useSupabase;
