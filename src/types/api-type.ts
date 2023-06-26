1db12c508102ded724f6d62a57bec1051bca3bb2
Daegyeom Ha <combbm@gmail.com>
2023-06-26 18:39:03 +0900

    fix: remove typeorm and firebase codes

--- src/types/api-type.ts
+++
@@ -1,4 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import type { DataSource } from 'typeorm';
-
-export type NextApiWithDB = (req: NextApiRequest, res: NextApiResponse, datasource: DataSource) => Promise<void> | void;
