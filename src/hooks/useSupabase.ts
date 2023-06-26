f906c545ac5f5e9f81ce875f7360379f8715db9f
Daegyeom Ha <combbm@gmail.com>
2023-06-26 19:32:59 +0900

    chore: rename api key to `NEXT_PUBLIC_SUPABASE_ANON_KEY`

--- src/hooks/useSupabase.ts
+++ src/hooks/useSupabase.ts
@@ -3,7 +3,7 @@ import { useMemo } from 'react';
 import { Database } from '../types/schema';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL ?? '';
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_API_KEY ?? '';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '';
 
 const useSupabase = () => {
   return useMemo(() => createClient<Database>(supabaseUrl, supabaseKey), []);
