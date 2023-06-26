d6496a95ab9fb6cf467ab6c57e153a73466b5564
Daegyeom Ha <combbm@gmail.com>
2023-06-26 18:54:32 +0900

    feat: add error toast in main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from '@emotion/styled';
 import { useHotkeys } from 'react-hotkeys-hook';
 import { useRouter } from 'next/router';
@@ -9,6 +9,7 @@ import Footer from '../components/Footer';
 import Colors from '../styles/Colors';
 import Content from '../components/Content';
 import { supabaseClient } from '../hooks/useSupabase';
+import { useToast } from '@chakra-ui/react';
 
 const Container = styled.div`
   margin: 3rem 10rem 0;
@@ -31,16 +32,32 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
     .order('id', { ascending: true });
   return {
     props: {
-      sections: data
+      sections: data,
+      error
     }
   };
 };
 
-const Index: React.FC = ({ sections }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const Index: React.FC = ({ sections, error }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const router = useRouter();
+  const toast = useToast({
+    isClosable: true,
+    position: 'top-left'
+  });
+
   useHotkeys('a+d', () => {
     router.push('/admin');
   });
+
+  useEffect(() => {
+    if (error !== null) {
+      toast({
+        title: '오류',
+        description: '정보를 불러올 수 없습니다. 잠시후 다시 시도해주세요.',
+        status: 'error'
+      });
+    }
+  }, []);
 
   return (
     <>
