f523f7ca8059d79163cd768e61a684cc09f5a3e8
Daegyeom Ha <combbm@gmail.com>
2023-06-26 18:42:48 +0900

    fix: remove unnecessary deps array

--- src/hooks/useSupabase.ts
+++ src/hooks/useSupabase.ts
@@ -6,7 +6,7 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL ?? '';
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_API_KEY ?? '';
 
 const useSupabase = () => {
-  return useMemo(() => createClient<Database>(supabaseUrl, supabaseKey), [supabaseUrl, supabaseKey]);
+  return useMemo(() => createClient<Database>(supabaseUrl, supabaseKey), []);
 };
 
 export const supabaseClient = createClient<Database>(supabaseUrl, supabaseKey);
