0de26b9e438b2ea5638f442b3a4a2b0038cbae1d
Daegyeom Ha <combbm@gmail.com>
2024-04-28 14:13:49 +0900

    fix: update `meta` and `script` tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import Head from 'next/head';
-import Script from 'next/script';
 import { createPagesBrowserClient, Session } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import GlobalStyle from '../styles/GlobalStyle';
@@ -25,27 +24,7 @@ const App = ({
     <>
       <Head>
         <title>하대겸 | Daegyeom Ha</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Script
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2490453096003621"
-        strategy="afterInteractive"
-        crossOrigin="anonymous"
-      />
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-J0FQZH7MNY" strategy="afterInteractive" />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-  
-            gtag('config', 'G-J0FQZH7MNY');
-            `
-        }}
-      />
       <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
         <ChakraProvider>
           <GlobalStyle />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import { Head, Html, Main, NextScript } from 'next/document';
+import { GoogleAnalytics } from '@next/third-parties/google';
 import Colors from '../styles/Colors';
 
 const Document = () => {
   return (
     <Html lang="ko">
       <Head>
+        <script
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2490453096003621"
+          crossOrigin="anonymous"
+          async
+        />
+        <GoogleAnalytics gaId="G-J0FQZH7MNY" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
         <meta charSet="utf-8" />
+
         <link rel="icon" href="favicon.ico" />
         <link rel="apple-touch-icon" href="favicon.ico" />
+
         <meta name="author" content="하대겸" />
-        <meta name="keywords" content="Daegyeom Ha, 하대겸, Resume, Portfolio, 포트폴리오" />
+        <meta name="keywords" content="Daegyeom Ha, 하대겸, daegyeome, daegyeo.me" />
         <meta name="theme-color" content={Colors.PRIMARY} />
         <meta name="description" content="Resume of Daegyeom Ha" />
+
         <meta property="og:site_name" content="하대겸 | Daegyeom Ha" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="하대겸 | Daegyeom Ha" />
