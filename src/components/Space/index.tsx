2671428e9d39a29485816ef19c47a638d2d5df52
Daegyeom Ha <combbm@gmail.com>
2024-08-23 21:21:39 +0900

    feat: implement Space component

---
+++ src/components/Space/index.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react';
+
+interface SpaceProps {
+  readonly x?: number;
+  readonly y?: number;
+}
+
+export const Space: FC<SpaceProps> = ({ x, y }) => {
+  return (
+    <div
+      style={{
+        marginBottom: `${y}px` ?? 0,
+        marginRight: `${x}px` ?? 0
+      }}
+    />
+  );
+};
