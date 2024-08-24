dede531d037ddcb321b3c701601df1f4b5928912
Daegyeom Ha <combbm@gmail.com>
2024-08-24 14:44:51 +0900

    feat: implement typography

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import Colors from '../../styles/Colors';
 
 /* Legacy */
 export const Title = styled.p`
@@ -9,4 +10,33 @@ export const Title = styled.p`
 export const HugeTitle = styled.p`
   font-size: 30px;
   font-weight: bold;
+`;
+
+/* Redesign */
+export const SectionTitle = styled.h1`
+  font-size: 24px;
+  font-weight: 700;
+  color: ${Colors.PRIMARY};
+`;
+
+export const LargeContentText = styled.p`
+  font-size: 20px;
+`;
+
+export const LargeHintedText = styled.span`
+  font-size: 16px;
+  color: ${Colors.GRAY_DARKEN};
+`;
+
+export const ContentListItem = styled.li`
+  width: 630px;
+  font-size: 16px;
+  word-break: break-all;
+  padding-left: 12px;
+  text-indent: -12px;
+  list-style-type: none;
+
+  &::before {
+    content: '- ';
+  }
 `;
