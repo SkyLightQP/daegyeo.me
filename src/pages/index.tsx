55e92eb3b08ff7e525763dbd569927c807ea987e
Daegyeom Ha <combbm@gmail.com>
2024-08-24 15:08:38 +0900

    refactor: redesign ui and structure of index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,24 +4,76 @@ import { useHotkeys } from 'react-hotkeys-hook';
 import { useRouter } from 'next/router';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { useToast } from '@chakra-ui/react';
+import { RiPagesLine } from '@remixicon/react';
 import Landing from '../components/Landing';
-import MoreLink from '../components/MoreLink';
-import Footer from '../components/Footer';
-import Colors from '../styles/Colors';
-import Content from '../components/Content';
 import { supabaseClient } from '../utils/supabase';
+import { Space } from '../components/Space';
+import { ExternalLink } from '../components/Link/ExternalLink';
+import { ContentListItem, LargeContentText, LargeHintedText, SectionTitle } from '../components/Typography';
+import { SocialLink } from '../components/Link/SocialLink';
 
 const Container = styled.div`
-  margin: 3rem 10rem 0;
-  color: ${Colors.PRIMARY};
+  margin: 10rem 172px;
 
   :lang(ko) {
     word-break: keep-all;
   }
 
-  @media screen and (max-width: 420px) {
-    margin: 3rem 2rem 0 2rem;
+  & > div {
+    margin-bottom: 64px;
   }
+
+  & > div:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+const ContentList = styled.div`
+  & > div {
+    margin-bottom: 28px;
+  }
+
+  & > div:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+const ExternalLinkGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  & > a {
+    margin-right: 6px;
+  }
+
+  & > a:last-child {
+    margin-right: 0;
+  }
+`;
+
+const ImageGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  & > img {
+    margin-right: 16px;
+  }
+
+  & > img:last-child {
+    margin-right: 0;
+  }
+`;
+
+const StyledImage = styled.img`
+  width: auto;
+  height: 80px;
+  border-radius: 10px;
+`;
+
+const SocialLinkGroup = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 0.15fr);
+  grid-row-gap: 4px;
 `;
 
 export const getServerSideProps: GetServerSideProps = async () => {
@@ -64,9 +116,60 @@ const Index: React.FC = ({ sections, error }: InferGetServerSidePropsType<typeof
       <Landing />
 
       <Container>
-        <Content sections={sections} />
-        <MoreLink />
-        <Footer />
+        <div>
+          <SectionTitle>이러한 기술을 활용합니다. — 기술스택</SectionTitle>
+          <Space y={6} />
+          <LargeContentText>- TypeScript, Kotlin, Java</LargeContentText>
+          <LargeContentText>- TypeScript, Kotlin, Java</LargeContentText>
+          <LargeContentText>- TypeScript, Kotlin, Java</LargeContentText>
+        </div>
+        <div>
+          <SectionTitle>이러한 곳에서 함께 했습니다. — 경력</SectionTitle>
+          <Space y={6} />
+          <ContentList>
+            <div>
+              <LargeContentText>
+                개발자 <LargeHintedText>(개발팀, 2024.01. ~ 2024.04.)</LargeHintedText>
+              </LargeContentText>
+              <ContentListItem>
+                이곳에는 리스트 형태의 내용이 들어갑니다. 마크다운에서는 * 또는 -으로 표현할 수 있습니다.
+              </ContentListItem>
+              <Space y={6} />
+              <ImageGroup>
+                <StyledImage src="https://via.placeholder.com/1920x1080" alt="이미지" />
+                <StyledImage src="https://via.placeholder.com/1920x1080" alt="이미지" />
+                <StyledImage src="https://via.placeholder.com/1920" alt="이미지" />
+                <StyledImage src="https://via.placeholder.com/1920" alt="이미지" />
+                <StyledImage src="https://via.placeholder.com/1920" alt="이미지" />
+              </ImageGroup>
+              <Space y={6} />
+              <ExternalLinkGroup>
+                <ExternalLink href="#">블로그</ExternalLink>
+                <ExternalLink href="#">홈페이지</ExternalLink>
+              </ExternalLinkGroup>
+            </div>
+          </ContentList>
+        </div>
+        <div>
+          <SectionTitle>더 궁금하시다면,</SectionTitle>
+          <Space y={6} />
+          <LargeContentText>
+            <SocialLinkGroup>
+              <SocialLink href="#" icon={RiPagesLine}>
+                블로그
+              </SocialLink>
+              <SocialLink href="#" icon={RiPagesLine}>
+                블로그
+              </SocialLink>
+              <SocialLink href="#" icon={RiPagesLine}>
+                블로그
+              </SocialLink>
+              <SocialLink href="#" icon={RiPagesLine}>
+                블로그
+              </SocialLink>
+            </SocialLinkGroup>
+          </LargeContentText>
+        </div>
       </Container>
     </>
   );
