00f42252509580b2a01a306b88acb5775bcc0f46
Daegyeom Ha <combbm@gmail.com>
2024-08-26 20:28:35 +0900

    feat: implement ImageView component

---
+++ src/components/ContentView/ImageView/index.tsx
@@ -0,0 +1,41 @@
+import React, { FC } from 'react';
+import styled from '@emotion/styled';
+import Image from 'next/image';
+import Breakpoint from '../../../styles/Breakpoint';
+
+interface ImageViewProps {
+  readonly url: string[];
+}
+
+const ImageGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  & > img {
+    margin-right: 16px;
+  }
+
+  & > img:last-child {
+    margin-right: 0;
+  }
+
+  @media screen and (max-width: ${Breakpoint.MOBILE}) {
+    overflow-x: auto;
+  }
+`;
+
+const StyledImage = styled(Image)`
+  width: auto;
+  height: 80px;
+  border-radius: 10px;
+`;
+
+export const ImageView: FC<ImageViewProps> = ({ url }) => {
+  return (
+    <ImageGroup>
+      {url.map((src) => (
+        <StyledImage key={src} src={src} alt="프로젝트 썸네일" width="147" height="80" />
+      ))}
+    </ImageGroup>
+  );
+};
