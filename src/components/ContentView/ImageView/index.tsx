087edb8b4c8cb91f5d2cf4975f7dd57f48ebd935
Daegyeom Ha <combbm@gmail.com>
2024-09-12 20:56:28 +0900

    feat: show ImageDetailModal when click image

--- src/components/ContentView/ImageView/index.tsx
+++ src/components/ContentView/ImageView/index.tsx
@@ -1,7 +1,9 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import styled from '@emotion/styled';
 import Image from 'next/image';
+import { useDisclosure } from '@chakra-ui/react';
 import Breakpoint from '../../../styles/Breakpoint';
+import ImageDetailModal from '../../Dialogs/ImageDetailModal';
 
 interface ImageViewProps {
   readonly images: { url: string; alt: string }[];
@@ -27,15 +29,30 @@ const ImageGroup = styled.div`
 const StyledImage = styled(Image)`
   width: auto;
   height: 80px;
-  border-radius: 10px;
+  border-radius: 6px;
+  cursor: pointer;
 `;
 
 export const ImageView: FC<ImageViewProps> = ({ images }) => {
+  const modal = useDisclosure();
+  const [modalContext, setModalContext] = useState({
+    url: '',
+    alt: ''
+  });
+
+  const onImageClick = (url: string, alt: string) => {
+    setModalContext({ url, alt });
+    modal.onOpen();
+  };
+
   return (
-    <ImageGroup>
-      {images.map(({ url, alt }) => (
-        <StyledImage key={url} src={url} alt={alt} width="147" height="80" />
-      ))}
-    </ImageGroup>
+    <>
+      <ImageGroup>
+        {images.map(({ url, alt }) => (
+          <StyledImage key={url} src={url} alt={alt} width="147" height="80" onClick={() => onImageClick(url, alt)} />
+        ))}
+      </ImageGroup>
+      <ImageDetailModal modalController={modal} image={modalContext} />
+    </>
   );
 };
