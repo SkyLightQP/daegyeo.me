e2a81fdd6f97e60b0f424de0dc223ff017499a72
Daegyeom Ha <combbm@gmail.com>
2024-11-07 14:12:44 +0900

    feat: add database type in supabase client factory

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,7 +1,8 @@
 import { createBrowserClient } from '@supabase/ssr';
+import { Database } from '../../types/schema';
 
 export function createSupabaseClient() {
-  return createBrowserClient(
+  return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ''
   );
--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,10 +1,11 @@
 import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
+import { Database } from '../../types/schema';
 
 export async function createSupabaseClient() {
   const cookieStore = await cookies();
 
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
     {
