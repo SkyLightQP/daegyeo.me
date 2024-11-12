650670c3bef0a90cf6b7141be556ce144269b346
Daegyeom Ha <combbm@gmail.com>
2024-11-12 10:19:14 +0900

    fix: remove unused file

--- src/constants/seo.ts
+++
@@ -1,9 +0,0 @@
-import { DefaultSeoProps } from 'next-seo';
-
-export const DEFAULT_SEO: DefaultSeoProps = {
-  title: '하대겸 | Daegyeom Ha',
-  description: '우리 생활을 더 편리하게 만듭니다. 개발자 하대겸입니다.',
-  canonical: 'https://daegyeo.me',
-  themeColor: '#35425A',
-
-};
