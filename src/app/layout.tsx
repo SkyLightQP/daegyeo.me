2b16da127b899052c07118a11b34fd6d1a495eae
Daegyeom Ha <combbm@gmail.com>
2026-04-19 23:41:15 +0900

    feat: add Pretendard font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import { FC, PropsWithChildren } from 'react';
+import localFont from 'next/font/local';
 import './globals.css';
+
+const pretendard = localFont({
+  src: './fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+});
 
 const RootLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
@@ -8,7 +15,9 @@ const RootLayout: FC<PropsWithChildren> = ({ children }) => {
         <title>title</title>
         <meta charSet="utf-8" />
       </head>
-      <body>{children}</body>
+      <body className={`${pretendard.className} bg-gray-100 text-zinc-900`}>
+        <div className="mx-auto w-full min-w-132.5 max-w-220 py-24">{children}</div>
+      </body>
     </html>
   );
 };
