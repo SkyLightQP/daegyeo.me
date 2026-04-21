905f5d2f6b5623c34a31a05d4ff320a79bae5f14
Daegyeom Ha <combbm@gmail.com>
2026-04-22 00:11:05 +0900

    fix: fix container layout with background effects

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,9 @@ const RootLayout: FC<PropsWithChildren> = ({ children }) => {
         <title>title</title>
         <meta charSet="utf-8" />
       </head>
-      <body className={`${pretendard.className} bg-gray-100 text-zinc-900`}>
-        <div className="mx-auto w-full min-w-132.5 max-w-220 py-24">{children}</div>
+      <body className={`relative ${pretendard.className} bg-gray-50 text-zinc-900`}>
+        <div className="mx-auto w-full min-w-132.5 max-w-200 px-6 py-24 sm:px-0">{children}</div>
+        <div className="pointer-events-none absolute bottom-0 right-0 h-128 w-lg -mr-20 sm:mr-0 bg-linear-to-tl from-blue-200/25 via-sky-100/15 to-transparent blur-3xl" />
       </body>
     </html>
   );
