e52206daacfbe85cc2a9a8567a5ceebef6066191
Daegyeom Ha <combbm@gmail.com>
2026-04-27 23:00:14 +0900

    fix: adjust background effect opacity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ const RootLayout: FC<PropsWithChildren> = ({ children }) => {
     <html lang="ko">
       <body className={`relative ${pretendard.className} bg-gray-50 text-zinc-900`}>
         <div className="mx-auto w-full min-w-132.5 max-w-200 px-6 py-24 sm:px-0">{children}</div>
-        <div className="pointer-events-none absolute bottom-0 right-0 h-128 w-lg -mr-20 sm:mr-0 bg-linear-to-tl from-blue-200/25 via-sky-100/15 to-transparent blur-3xl" />
+        <div className="pointer-events-none absolute bottom-0 right-0 h-128 w-lg -mr-20 sm:mr-0 bg-linear-to-tl from-blue-200/30 via-sky-100/15 to-transparent blur-3xl" />
       </body>
     </html>
   );
