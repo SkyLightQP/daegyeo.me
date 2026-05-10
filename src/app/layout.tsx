ae57d89ed95071547d4ebc8bc0a2fba538bb7ac7
Daegyeom Ha <combbm@gmail.com>
2026-05-11 00:10:01 +0900

    feat: add shadcn for admin page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,12 +30,13 @@ const pretendard = localFont({
   src: './fonts/PretendardVariable.woff2',
   display: 'swap',
   weight: '45 920',
+  variable: '--font-sans',
 });
 
 const RootLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <html lang="ko">
-      <body className={`relative ${pretendard.className} bg-gray-50 text-zinc-900`}>
+    <html lang="ko" className={pretendard.variable}>
+      <body className="relative bg-gray-50 text-zinc-900">
         <div className="mx-auto w-full min-w-132.5 max-w-200 px-6 py-24 sm:px-0">{children}</div>
         <div className="pointer-events-none absolute bottom-0 right-0 h-128 w-lg -mr-20 sm:mr-0 bg-linear-to-tl from-blue-200/30 via-sky-100/15 to-transparent blur-3xl" />
       </body>
