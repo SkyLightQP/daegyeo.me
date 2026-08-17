61850dd122d46efb0b651c26e8aff604249310a0
Daegyeom Ha <combbm@gmail.com>
2026-08-17 21:25:51 +0900

    feat: add break-keep

--- src/components/blocks/Introduce.tsx
+++ src/components/blocks/Introduce.tsx
@@ -6,7 +6,7 @@ export const Introduce = () => {
       <h1 className="text-3xl font-bold">
         하대겸 <span className="text-2xl font-semibold">Daegyeom Ha</span>
       </h1>
-      <p className="text-lg font-medium mt-1">
+      <p className="text-lg font-medium mt-1 break-keep">
         안녕하세요, 소프트웨어 엔지니어 하대겸입니다. 분야 상관없이 기술로 문제를 해결하는 일을 즐깁니다. 신뢰성 높고
         유지보수하기 쉬운 시스템을 만들고 있습니다.
       </p>
