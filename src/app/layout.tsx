e23463f93ead661e3d9632178c0534b978ac2479
Daegyeom Ha <combbm@gmail.com>
2026-08-17 21:45:04 +0900

    feat: add overflow-x-hidden style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { FC, PropsWithChildren } from 'react';
 import localFont from 'next/font/local';
 import type { Metadata } from 'next';
+import { cn } from '@/lib/utils';
 import './globals.css';
 
 const SITE_URL = 'https://daegyeo.me';
@@ -35,8 +36,8 @@ const pretendard = localFont({
 
 const RootLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <html lang="ko" className={pretendard.variable}>
-      <body className="relative bg-gray-50 text-zinc-900">{children}</body>
+    <html lang="ko" className={cn(pretendard.variable, 'overflow-x-hidden')}>
+      <body className="relative overflow-x-hidden bg-gray-50 text-zinc-900">{children}</body>
     </html>
   );
 };
